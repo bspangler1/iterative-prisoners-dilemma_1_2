3deddb24986467958fdb65e3453ac298d0df6a21
bspangler1 <braxton.spangler@students.sacs.k12.in.us>
2015-12-17 08:04:10 -0500

    Update prisoners_dilemma.py

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -549,12 +549,12 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 18:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'GoFace'
         else:
-            if len(opponent_history)==0: #It's the first round: collude
-                return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were severely punished last time
+            if len(opponent_history)==0: #It's the first round: betray
+                return 'b'
+            elif history[-1]=='c' and opponent_history[-2]=='c':
+                return 'b' # betray if they colluded twice in a row
             else:
                 return 'c' #otherwise collude
     
